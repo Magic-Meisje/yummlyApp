01e6efff263fd88e1dfe5413f5780a5c1ddd608e
Amie Phipps <amiejoyphipps@gmail.com>
2015-10-21 21:29:35 -0400

    updated javascript

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,6 @@ var yummlyApp = {};
 
 
 yummlyApp.getRecipes = function() {
-	var vegan = $().val();
-	var paleo = $().val();
-	var vegetarian = $().val();
-	var glutenFree = $().val();
-	var dairy = $().val();
-	var wheat = $().val();
-	var soy = $().val();
-	var seafood = $().val();
-
 	$.ajax({
 		url: 'https://api.yummly.com/v1/api/recipes',
 		dataType: 'jsonp',
@@ -19,24 +10,34 @@ yummlyApp.getRecipes = function() {
 		data: {
 			_app_id: '051fb3a8',
 			_app_key: '0827bd8424e3ad1432cc27d19823deda',
-			allowedCourse: 'Main Dishes',
-			allowedDiet: vegetarian,
-			allowedDiet: vegan,
-			allowedAllergy: glutenFree,
-			allowedAllergy: dairy,
-			allowedAllergy: wheat,
-			allowedAllergy: soy,
-			allowedAllergy: seafood
+			allowedCourse: 'course^course-Main Dishes',
+			allowedDiet: yummlyApp.userDietSelection,
+			allowedAllergy: yummlyApp.userAllergySelection
 		}
 	}).then(function(recipes) {
+		// yummylyApp.returnedRecipes = recipes;
 		console.log(recipes);
 	});
 };
 
+yummlyApp.userDietarySubmit = function() {
+	// var userAllergySelection = null;
+	// var userDietSelection = null;
 
+	$("#dietarySelection").on("submit", function(e) {
+		e.preventDefault();
+		if ($('#diets').val() === 'vegan' || 'vegetarian') {
+			yummlyApp.userDietSelection = $("#myDropdown").val();
+		} else if ($('#diets').val() === 'glutenFree' || 'dairy') {
+			yummlyApp.userAllergySelection = $("#myDropdown").val();
+		}
+
+		yummlyApp.getRecipes();
+	});
+};
 
 yummlyApp.init = function() {
-	yummlyApp.getRecipes();
+	yummlyApp.userDietarySubmit();
 };
 
 $(function() {
