821249e9335d3bd9c5493756f13c08bcc1ce99ad
Tom Haxell <tom.haxell@gmail.com>
2015-10-22 16:40:09 -0400

    created the correct ajax call

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 var yummlyApp = {};
 
+var userAllergySelection = null;
+
+var userSearch = null;
+
+
 
 
 yummlyApp.getRecipes = function() {
@@ -11,9 +16,11 @@ yummlyApp.getRecipes = function() {
 		data: {
 			_app_id: '051fb3a8',
 			_app_key: '0827bd8424e3ad1432cc27d19823deda',
-			allowedCourse: 'course^course-Main Dishes',
-			allowedDiet: yummlyApp.userDietSelection,
-			allowedAllergy: yummlyApp.userAllergySelection
+			requirePictures: true,
+			q: userSearch,
+			allowedDiet: userDietSelection,
+			allowedAllergy: userAllergySelection,
+			allowedCourse: 'course^course-Main Dishes'
 		}
 	}).then(function(recipes) {
 		// yummylyApp.returnedRecipes = recipes;
@@ -21,24 +28,25 @@ yummlyApp.getRecipes = function() {
 	});
 };
 
-yummlyApp.userDietarySubmit = function() {
-	// var userAllergySelection = null;
-	// var userDietSelection = null;
+// yummlyApp.userDietarySubmit = function() {
+// 	// var userAllergySelection = null;
+// 	// var userDietSelection = null;
 
-	$("#dietarySelection").on("submit", function(e) {
-		e.preventDefault();
-		if ($('#diets').val() === 'vegan' || 'vegetarian') {
-			yummlyApp.userDietSelection = $("#myDropdown").val();
-		} else if ($('#diets').val() === 'glutenFree' || 'dairy') {
-			yummlyApp.userAllergySelection = $("#myDropdown").val();
-		}
+// 	$("#dietarySelection").on("submit", function(e) {
+// 		e.preventDefault();
+// 		if ($('#diets').val() === 'vegan' || 'vegetarian') {
+// 			yummlyApp.userDietSelection = $("#myDropdown").val();
+// 		} else if ($('#diets').val() === 'glutenFree' || 'dairy') {
+// 			yummlyApp.userAllergySelection = $("#myDropdown").val();
+// 		}
 
-		yummlyApp.getRecipes();
-	});
-};
+// 		yummlyApp.getRecipes();
+// 	});
+// };
 
 yummlyApp.init = function() {
-	yummlyApp.userDietarySubmit();
+	// yummlyApp.userDietarySubmit();
+	yummlyApp.getRecipes();
 };
 
 $(function() {
