e505977f14ff6447439aca745234f7d8142f728c
Tom Haxell <tom.haxell@gmail.com>
2015-10-22 14:26:08 -0400

    deleted some files

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,8 @@ var yummlyApp = {};
 
 
 yummlyApp.getRecipes = function() {
-	var vegan = $().val();
-	var paleo = $().val();
-	var vegetarian = $().val();
-	var glutenFree = $().val();
-	var dairy = $().val();
-	var wheat = $().val();
-	var soy = $().val();
-	var seafood = $().val();
+	var UserDiet = null;
+	var UserAllergy = null;
 
 	$.ajax({
 		url: 'https://api.yummly.com/v1/api/recipes',
@@ -19,14 +13,9 @@ yummlyApp.getRecipes = function() {
 		data: {
 			_app_id: '051fb3a8',
 			_app_key: '0827bd8424e3ad1432cc27d19823deda',
-			allowedCourse: 'Main Dishes',
-			allowedDiet: vegetarian,
-			allowedDiet: vegan,
-			allowedAllergy: glutenFree,
-			allowedAllergy: dairy,
-			allowedAllergy: wheat,
-			allowedAllergy: soy,
-			allowedAllergy: seafood
+			allowedCourse: 'course^course-Main Dishes',
+			allowedDiet: UserDiet,
+			allowedAllergy: UserAllergy
 		}
 	}).then(function(recipes) {
 		console.log(recipes);
