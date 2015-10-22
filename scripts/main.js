9fe070c954f2653bed479bb643fa34ec4b81a51d
staciecampbell <staciercampbell@gmail.com>
2015-10-22 16:44:12 -0400

    more changes

--- scripts/main.js
+++ scripts/main.js
@@ -12,32 +12,36 @@ yummlyApp.getRecipes = function() {
 			_app_key: '0827bd8424e3ad1432cc27d19823deda',
 			allowedCourse: 'course^course-Main Dishes',
 			allowedDiet: yummlyApp.userDietSelection,
-			allowedAllergy: yummlyApp.userAllergySelection
+			allowedAllergy: yummlyApp.userAllergySelection,
+			q: yummlyApp.userInputSearch
 		}
 	}).then(function(recipes) {
-		// yummylyApp.returnedRecipes = recipes;
 		console.log(recipes);
 	});
 };
 
 yummlyApp.userDietarySubmit = function() {
-	// var userAllergySelection = null;
-	// var userDietSelection = null;
-
 	$("#dietarySelection").on("submit", function(e) {
 		e.preventDefault();
 		if ($('#diets').val() === 'vegan' || 'vegetarian') {
 			yummlyApp.userDietSelection = $("#myDropdown").val();
 		} else if ($('#diets').val() === 'glutenFree' || 'dairy') {
 			yummlyApp.userAllergySelection = $("#myDropdown").val();
-		}
-
+		} 
+	
+		yummlyApp.userInputSearch = $('#userSearch').val();
+		console.log(yummlyApp.userInputSearch);
+	
 		yummlyApp.getRecipes();
+
+		$('main section').removeClass('hide');
+		$('header').addClass('hide');
 	});
 };
 
 yummlyApp.init = function() {
 	yummlyApp.userDietarySubmit();
+
 };
 
 $(function() {
